14600c5b3a36a4a617eb4d06f5c9e90ac69adbe5
Vlad Zavadskii <vzavadskii@jaspersoft.com>
2012-07-23 21:37:43 +0000

    Profile copyright added

--- Classes/JSClient/JSServerProfile.h
+++ Classes/JSClient/JSServerProfile.h
@@ -1,9 +1,33 @@
+/*
+ * Jaspersoft Mobile SDK
+ * Copyright (C) 2001 - 2012 Jaspersoft Corporation. All rights reserved.
+ * http://www.jasperforge.org/projects/mobile
+ *
+ * Unless you have purchased a commercial license agreement from Jaspersoft,
+ * the following license terms apply:
+ *
+ * This program is part of Jaspersoft Mobile SDK.
+ *
+ * Jaspersoft Mobile SDK is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Jaspersoft Mobile SDK is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FJaspersoft CorpOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with Jaspersoft Mobile SDK. If not, see <http://www.gnu.org/licenses/>.
+ */
+
 //
 //  JSServerProfile.h
 //  JaspersoftMobileSDK
 //
-//  Created by Volodya on 7/3/12.
-//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//  Created by Volodya Sabadosh on 7/3/12.
+//  Copyright (c) 2012 Jaspersoft Corp. All rights reserved.
 //
 
 #import <Foundation/Foundation.h>
