4af0e0c9b85b80f05927e34a3f7b7a08762db687
Olexandr Dahno <aleksandr.dakhno@gmail.com>
2015-10-16 11:20:21 +0300

    create new class for JSErrorBuilder.

---
+++ Classes/Helpers/JSErrorBuilder/JMErrorBuilder.h
@@ -0,0 +1,9 @@
+//
+// Created by Aleksandr Dakhno on 10/16/15.
+//
+
+#import <Foundation/Foundation.h>
+
+
+@interface JMErrorBuilder : NSObject
+@end
---
+++ Classes/Helpers/JSErrorBuilder/JMErrorBuilder.m
@@ -0,0 +1,11 @@
+//
+// Created by Aleksandr Dakhno on 10/16/15.
+//
+
+#import "JMErrorBuilder.h"
+
+
+@implementation JMErrorBuilder {
+
+}
+@end
