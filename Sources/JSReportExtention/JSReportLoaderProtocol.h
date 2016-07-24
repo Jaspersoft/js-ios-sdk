8e2b88e0714991bceb7aa8692a08719639f275d2
Olexandr Dahno <odahno@tibco.com>
2016-07-24 18:32:58 +0300

    update JSReportLoaderErrorType enumeration in JSReportLoaderProtocol.

--- Sources/JSReportExtention/JSReportLoaderProtocol.h
+++ Sources/JSReportExtention/JSReportLoaderProtocol.h
@@ -40,7 +40,8 @@
 typedef NS_ENUM(NSInteger, JSReportLoaderErrorType) {
     JSReportLoaderErrorTypeUndefined,
     JSReportLoaderErrorTypeEmtpyReport,
-    JSReportLoaderErrorTypeAuthentification,
+    JSReportLoaderErrorTypeSessionDidExpired,
+    JSReportLoaderErrorTypeSessionDidRestore,
     JSReportLoaderErrorTypeLoadingCanceled
 };
 
