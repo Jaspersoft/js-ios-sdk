f02e40339e552db4b921d3650048fbfe185c5116
Alexey Gubarev <gubarev.lesha@gmail.com>
2016-11-30 14:09:48 +0200

    Corrected importing headers for subspecs

--- Sources/JSHelper/JaspersoftSDK.h
+++ Sources/JSHelper/JaspersoftSDK.h
@@ -38,6 +38,10 @@
 #import "JSReportExtention.h"
 #endif
 
+#if __has_include("JSDashboardExtention.h")
+#import "JSDashboardExtention.h"
+#endif
+
 #if __has_include("JSSecurity.h")
 #import "JSSecurity.h"
 #endif
